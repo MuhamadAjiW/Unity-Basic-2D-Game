be8e9ad521fa42cd92cce08bb8470661a8e9fc3a
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-12-20 23:32:00 +0700

    fix: tweaked attack combo

--- Assets/Scripts/Library/Attack/TestWeapon.cs
+++ Assets/Scripts/Library/Attack/TestWeapon.cs
@@ -4,10 +4,17 @@ public class TestWeapon : WeaponObject{
     private const string ATTACK_TRIGGER = "Attacking"; 
     private const string STOP_TRIGGER = "Stopped"; 
 
-    private bool attackInput = false;
+    public bool attackInput = false;
+    
+    public bool finalAttack = false;
+
+    public bool FinalAttack{
+        get {return finalAttack;}
+        set {finalAttack = value;}
+    }
 
     public override void Attack(){
-        if(!attackInput){
+        if(!attackInput && !finalAttack){
             attackInput = true;
             animator.SetBool(ATTACK_TRIGGER, true);
             animator.SetBool(STOP_TRIGGER, false);
@@ -15,10 +22,19 @@ public class TestWeapon : WeaponObject{
     }
 
     public void AttackDone(){
+        Debug.Log("Attack is done");
+        finalAttack = false;
+        attackInput = false;
         animator.SetBool(STOP_TRIGGER, true);
     }
 
     public void AttackCalled(){
+        attackInput = false;
+        animator.SetBool(ATTACK_TRIGGER, false);
+    }
+
+    public void FinalAttackCalled(){
+        finalAttack = true;
         attackInput = false;
         animator.SetBool(ATTACK_TRIGGER, false);
     }
